f61efcbb71b434724f98529e03e0d4b5cd76a9ce
DVadim001 <dvadim001@gmail.com>
2024-03-02 19:21:21 +0500

    LeetCode

--- LeetCode/main.py
+++ LeetCode/main.py
@@ -1,7 +1,6 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-        elif x == 0:
-            return True
-        else:
+    def romanToInt(self, s: str) -> int:
+        nums = []
+        romanian = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        romanian_nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
